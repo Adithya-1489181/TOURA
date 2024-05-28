ead928f179cf5ddb65f586fb163af3235df17467
kshku <1211shree@gmail.com>
2024-05-28 20:20:26 +0530

    Chatbot page is now ready to send text messages

--- toura/lib/UI/chat_screen/chat_page.dart
+++ toura/lib/UI/chat_screen/chat_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:toura/UI/chat_screen/chat_box.dart';
+import 'package:toura/UI/chat_screen/input_box.dart';
 
 class Message {
   String content;
@@ -9,64 +10,65 @@ class Message {
   Message({required this.content, required this.isFromUser});
 }
 
-List<Message> msgs = <Message>[
-  Message(content: "Hello", isFromUser: true),
-  Message(content: "content", isFromUser: false),
-  Message(
-      content:
-          "Eu velit et cupidatat mollit labore esse ea aute deserunt quis et laboris ea.",
-      isFromUser: true),
-  Message(
-      content:
-          """Deserunt est laboris exercitation nulla aliquip. Laborum officia sit elit qui dolore qui laborum aliqua aliqua id cillum voluptate et dolore. Laborum cupidatat voluptate est aliqua. Pariatur est est cupidatat Lorem tempor consequat aliquip tempor nostrud eiusmod voluptate proident est velit.
+class MsgNotifier extends Notifier<List<Message>> {
+  @override
+  List<Message> build() {
+    return <Message>[
+      Message(content: "Hello", isFromUser: true),
+      Message(content: "content", isFromUser: false),
+      Message(
+          content:
+              "Eu velit et cupidatat mollit labore esse ea aute deserunt quis et laboris ea.",
+          isFromUser: true),
+      Message(
+          content:
+              """Deserunt est laboris exercitation nulla aliquip. Laborum officia sit elit qui dolore qui laborum aliqua aliqua id cillum voluptate et dolore. Laborum cupidatat voluptate est aliqua. Pariatur est est cupidatat Lorem tempor consequat aliquip tempor nostrud eiusmod voluptate proident est velit.
 
 Incididunt commodo consequat ex ipsum occaecat laborum veniam in labore ad. Quis irure culpa ipsum occaecat ad culpa eu eu sit anim minim anim officia. Excepteur dolor aliquip proident elit elit sint. Quis esse labore consequat occaecat sit consequat ipsum est proident qui aliquip elit. Id irure duis labore ad in. Non sint enim consequat irure pariatur.""",
-      isFromUser: false),
-];
+          isFromUser: false),
+    ];
+  }
 
-final messageProvider = Provider<List<Message>>(
-  (ref) {
-    return msgs;
-  },
-);
+  void add(Message msg) {
+    state = [...state, msg];
+  }
+}
 
-class ChatPage extends ConsumerWidget {
+final msgNotifierProvider = NotifierProvider<MsgNotifier, List<Message>>(() {
+  return MsgNotifier();
+});
+
+class ChatPage extends ConsumerStatefulWidget {
   const ChatPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    var msgs = ref.watch(messageProvider).reversed.toList();
+  ConsumerState<ChatPage> createState() => _ChatPageState();
+}
 
-    return Column(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            reverse: true,
-            shrinkWrap: true,
-            itemCount: msgs.length,
-            itemBuilder: (context, index) {
-              return ChatBox(
-                  message: msgs[index].content,
-                  isFromUser: msgs[index].isFromUser);
-            },
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 10),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(25),
-              ),
-              prefixIcon: const Icon(Icons.message),
-              suffixIcon: IconButton(
-                icon: const Icon(Icons.send),
-                onPressed: () {},
-              ),
+class _ChatPageState extends ConsumerState<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    var msgs = ref.watch(msgNotifierProvider).reversed.toList();
+
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              reverse: true,
+              shrinkWrap: true,
+              itemCount: msgs.length,
+              itemBuilder: (context, index) {
+                return ChatBox(
+                    message: msgs[index].content,
+                    isFromUser: msgs[index].isFromUser);
+              },
             ),
           ),
-        ),
-      ],
+          const InputBox(),
+        ],
+      ),
     );
   }
 }
